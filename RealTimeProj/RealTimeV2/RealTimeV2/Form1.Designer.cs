cb1af1ac9e6e67bf487c8b2f1e1e76303f91aaa8
unknown <kosov.r.m@gmail.com>
2020-01-14 00:19:49 +0300

    initial

---
+++ RealTimeProj/RealTimeV2/RealTimeV2/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace RealTimeV2
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+			this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.piRegulation = new System.Windows.Forms.RadioButton();
+			this.polynomialRegulator = new System.Windows.Forms.RadioButton();
+			this.desiredValueCtrl = new System.Windows.Forms.NumericUpDown();
+			this.inputCtrl = new System.Windows.Forms.NumericUpDown();
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.desiredValueCtrl)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.inputCtrl)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// chart1
+			// 
+			chartArea4.Name = "ChartArea1";
+			this.chart1.ChartAreas.Add(chartArea4);
+			this.chart1.Location = new System.Drawing.Point(12, 12);
+			this.chart1.Name = "chart1";
+			this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Berry;
+			series4.ChartArea = "ChartArea1";
+			series4.Name = "Series1";
+			this.chart1.Series.Add(series4);
+			this.chart1.Size = new System.Drawing.Size(514, 379);
+			this.chart1.TabIndex = 0;
+			this.chart1.Text = "chart1";
+			this.chart1.Click += new System.EventHandler(this.chart1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(532, 163);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(660, 119);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(136, 70);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Start";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(531, 18);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(31, 13);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Input";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(669, 18);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(72, 13);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "Desired value";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(532, 147);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(39, 13);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "Output";
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(535, 65);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(48, 17);
+			this.checkBox1.TabIndex = 8;
+			this.checkBox1.Text = "Auto";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+			// 
+			// piRegulation
+			// 
+			this.piRegulation.AutoSize = true;
+			this.piRegulation.Checked = true;
+			this.piRegulation.Enabled = false;
+			this.piRegulation.Location = new System.Drawing.Point(536, 95);
+			this.piRegulation.Name = "piRegulation";
+			this.piRegulation.Size = new System.Drawing.Size(79, 17);
+			this.piRegulation.TabIndex = 9;
+			this.piRegulation.TabStop = true;
+			this.piRegulation.Text = "PI-regulator";
+			this.piRegulation.UseVisualStyleBackColor = true;
+			this.piRegulation.CheckedChanged += new System.EventHandler(this.piRegulation_CheckedChanged);
+			// 
+			// polynomialRegulator
+			// 
+			this.polynomialRegulator.AutoSize = true;
+			this.polynomialRegulator.Enabled = false;
+			this.polynomialRegulator.Location = new System.Drawing.Point(535, 119);
+			this.polynomialRegulator.Name = "polynomialRegulator";
+			this.polynomialRegulator.Size = new System.Drawing.Size(119, 17);
+			this.polynomialRegulator.TabIndex = 10;
+			this.polynomialRegulator.Text = "Polynomial regulator";
+			this.polynomialRegulator.UseVisualStyleBackColor = true;
+			this.polynomialRegulator.CheckedChanged += new System.EventHandler(this.polynomialRegulator_CheckedChanged);
+			// 
+			// desiredValueCtrl
+			// 
+			this.desiredValueCtrl.Location = new System.Drawing.Point(668, 34);
+			this.desiredValueCtrl.Name = "desiredValueCtrl";
+			this.desiredValueCtrl.Size = new System.Drawing.Size(120, 20);
+			this.desiredValueCtrl.TabIndex = 12;
+			this.desiredValueCtrl.ValueChanged += new System.EventHandler(this.desiredValueCtrl_ValueChanged);
+			// 
+			// inputCtrl
+			// 
+			this.inputCtrl.DecimalPlaces = 10;
+			this.inputCtrl.Location = new System.Drawing.Point(532, 34);
+			this.inputCtrl.Name = "inputCtrl";
+			this.inputCtrl.Size = new System.Drawing.Size(120, 20);
+			this.inputCtrl.TabIndex = 11;
+			this.inputCtrl.ValueChanged += new System.EventHandler(this.inputCtrl_ValueChanged);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 413);
+			this.Controls.Add(this.desiredValueCtrl);
+			this.Controls.Add(this.inputCtrl);
+			this.Controls.Add(this.polynomialRegulator);
+			this.Controls.Add(this.piRegulation);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.chart1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.desiredValueCtrl)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.inputCtrl)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.RadioButton piRegulation;
+		private System.Windows.Forms.RadioButton polynomialRegulator;
+		private System.Windows.Forms.NumericUpDown desiredValueCtrl;
+		private System.Windows.Forms.NumericUpDown inputCtrl;
+	}
+}
+
---
+++ RealTimeProj/RealTimeV2/RealTimeV2/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace RealTimeV2
+{
+	public partial class Form1 : Form
+	{
+		static bool autoRegulation = false;
+		static int regulatType = -1;
+		static double goal = -1;
+		static double input = 0;
+		static double prevError = goal;
+		static double prevRes = 0;
+		static double kp = 0.9;
+		static double tu = 1.0 / 19.0;
+		static double T = 0.15;
+		static int i = 0;
+		Series mySeriesOfPoint;
+
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			if (chart1.InvokeRequired) chart1.Invoke(new Action(() => chart1.ChartAreas.Add(new ChartArea("Math functions"))));
+			else chart1.ChartAreas.Add(new ChartArea("Math functions"));
+
+			mySeriesOfPoint = new Series("Sirius");
+			mySeriesOfPoint.ChartType = SeriesChartType.Spline;
+			mySeriesOfPoint.ChartArea = "Math functions";
+			mySeriesOfPoint.BorderWidth = 3;
+
+			if (chart1.InvokeRequired) chart1.Invoke(new Action(() => chart1.Series.Add(mySeriesOfPoint)));
+			else chart1.Series.Add(mySeriesOfPoint);
+		}
+
+		private void calculation()
+		{
+			double h = 0.015;
+			double y1 = 0.0, y1new = 0.0;
+			double y2 = 0.0, y2new = 0.0;
+			double y3 = 0.0, y3new = 0.0;
+			double k1 = 0.0, k2 = 0.0, k3 = 0.0, k4 = 0.0;
+			double delta = 0.0;
+			double temp = 0;
+
+			while (i >= 0)
+			{
+				if (autoRegulation)
+				{
+					if (i % 10 == 0)
+					{
+						if (regulatType == 1)
+						{
+							temp = helpBlock2(y1);
+						}
+						else
+						{
+							temp = helpBlock(y1);
+						}
+					}
+				}
+				else
+				{
+					temp = input;
+				}
+
+				inputCtrl.Invoke(new Action(() => inputCtrl.Value = (decimal)input));
+				
+
+				k1 = h * y2;
+				k2 = h * (y2 + k1 / 2);
+				k3 = h * (y2 + k2 / 2);
+				k4 = h * (y2 + k3);
+				delta = 1.0 / 6.0 * (k1 + 2 * k2 + 2 * k3 + k4);
+				y1new = y1 + delta;
+
+				k1 = h * y3;
+				k2 = h * (y3 + k1 / 2);
+				k3 = h * (y3 + k2 / 2);
+				k4 = h * (y3 + k3);
+				delta = 1.0 / 6.0 * (k1 + 2 * k2 + 2 * k3 + k4);
+				y2new = y2 + delta;
+
+				k1 = h * (1.45 * temp - y1 - 140 * y3 - 25 * y2) / 322;
+				k2 = h * (1.45 * temp - y1 - 140 * (y3 + k1 / 2) - 25 * y2) / 322;
+				k3 = h * (1.45 * temp - y1 - 140 * (y3 + k2 / 2) - 25 * y2) / 322;
+				k4 = h * (1.45 * temp - y1 - 140 * (y3 + k3) - 25 * y2) / 322;
+				delta = 1.0 / 6.0 * (k1 + 2 * k2 + 2 * k3 + k4);
+				y3new = y3 + delta;
+
+				y1 = y1new;
+				y2 = y2new;
+				y3 = y3new;
+				i++;
+				//System.Threading.Thread.Sleep(100);
+
+				if (i % 100 == 0)
+				{
+					if (chart1.InvokeRequired) chart1.Invoke(new Action(() =>
+					{
+						if (i >= 0)
+							mySeriesOfPoint.Points.AddXY(i, y1);
+					}
+					));
+					else if (i >= 0)
+						mySeriesOfPoint.Points.AddXY(i, y1);
+
+
+					if (chart1.InvokeRequired) chart1.Invoke(new Action(() => textBox1.Text = y1.ToString()));
+					else textBox1.Text = y1.ToString();
+
+					if (chart1.InvokeRequired) chart1.Invoke(new Action(() =>
+					{
+						if (i >= 0)
+							chart1.Update();
+					}
+					));
+					else if (i >= 0)
+						chart1.Update();
+				}
+
+			}
+		}
+
+		public double helpBlock(double y1)
+		{
+			double curError = goal - y1;
+			//double temp = 0.904  * curError - 0.896 * prevError + prevRes;
+			double temp = kp * curError + (kp * tu * T - kp) * prevError + prevRes;
+			//double temp = (kp + kp*T*tu/2) * curError + (kp * T * tu / 2 - kp) * prevError + prevRes;
+			//double temp = kp*(1+T*tu) * curError - kp * prevError + prevRes;
+			prevRes = temp;
+			prevError = curError;
+			return temp;
+		}
+
+		public double helpBlock2(double y1)
+		{
+			double curError = goal - y1;
+			double temp = -1.056 * prevError + 1.066 * curError + prevRes;
+			prevRes = temp;
+			prevError = curError;
+			return temp;
+		}
+
+		private void chart1_Click(object sender, EventArgs e)
+		{
+			
+		}
+
+		private async void button1_Click(object sender, EventArgs e)
+		{
+			i = 0;
+			if (chart1.InvokeRequired) chart1.Invoke(new Action(() => mySeriesOfPoint.Points.Clear()));
+			else mySeriesOfPoint.Points.Clear();
+
+			autoRegulation = checkBox1.Checked;
+			if (piRegulation.Checked)
+			{
+				regulatType = 1;
+			}
+			else if (polynomialRegulator.Checked)
+			{
+				regulatType = 2;
+			}
+
+			goal = (double)desiredValueCtrl.Value;
+
+
+			await Task.Run(() => calculation());
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void inputCtrl_ValueChanged(object sender, EventArgs e)
+		{
+			input = (double)inputCtrl.Value;
+		}
+
+		private void checkBox1_CheckedChanged(object sender, EventArgs e)
+		{
+			autoRegulation = checkBox1.Checked;
+			piRegulation.Enabled = checkBox1.Checked;
+			polynomialRegulator.Enabled = checkBox1.Checked;
+			inputCtrl.Enabled = !checkBox1.Checked;
+		}
+
+		private void piRegulation_CheckedChanged(object sender, EventArgs e)
+		{
+			regulatType = 1;
+		}
+
+		private void polynomialRegulator_CheckedChanged(object sender, EventArgs e)
+		{
+			regulatType = 2;
+		}
+
+		private void desiredValueCtrl_ValueChanged(object sender, EventArgs e)
+		{
+			goal = (double)desiredValueCtrl.Value;
+		}
+	}
+}
